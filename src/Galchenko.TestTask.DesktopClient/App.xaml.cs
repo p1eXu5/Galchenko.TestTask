69328494ae49596277a25d47168320a990b8de89
p1eXu5 <v1adp1exu5@gmail.com>
2020-06-23 18:34:02 +0300

    chore: add user controls and styles

--- src/Galchenko.TestTask.DesktopClient/App.xaml.cs
+++ src/Galchenko.TestTask.DesktopClient/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Unity;
 
 namespace Galchenko.TestTask.DesktopClient
 {
@@ -13,5 +14,17 @@ namespace Galchenko.TestTask.DesktopClient
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup( StartupEventArgs e )
+        {
+            base.OnStartup( e );
+
+            var wnd = new MainWindow();
+            wnd.Show();
+        }
+
+        private void ConfigureServices( IUnityContainer container )
+        {
+
+        }
     }
 }
---
+++ src/Galchenko.TestTask.ViewModels/AppointmentListViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using p1eXu5.Wpf.MvvmLibrary;
+
+namespace Galchenko.TestTask.ViewModels
+{
+    public class AppointmentListViewModel : ViewModelBase
+    {
+        
+    }
+}
