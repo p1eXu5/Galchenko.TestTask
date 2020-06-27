f6b76a38a9dfd6e1ec42908e9f915f88d68aa5c4
p1eXu5 <v1adp1exu5@gmail.com>
2020-06-27 17:51:25 +0300

    cln

--- src/Galchenko.TestTask.ApplicationLayer/Appointments/Validators/AppointmentDtoBaseValidator.cs
+++ src/Galchenko.TestTask.ApplicationLayer/Appointments/Validators/AppointmentDtoBaseValidator.cs
@@ -8,7 +8,6 @@ namespace Galchenko.TestTask.ApplicationLayer.Appointments.Validators
     {
         public AppointmentDtoBaseValidator()
         {
-            var date = DateTime.Now;
             RuleFor( a => a.Date ).InclusiveBetween( new DateTimeOffset( 1971, 1, 1, 8, 0, 0, TimeSpan.Zero), DateTimeOffset.UtcNow + TimeSpan.FromDays( 830 ) );
             RuleFor( a => a.Diagnosis ).NotEmpty();
             RuleFor( a => a.Type ).IsInEnum();
